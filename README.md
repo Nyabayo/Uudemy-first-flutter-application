074f64056463df72fb2434711e06b42624a72424
Nyabayo <ernestosindo9@gmail.com>
2024-08-10 16:27:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# udemy1
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import   "package:flutter/material.dart";
+
+void main() {
+
+runApp(
+
+new Center(
+
+child: new Text("Hello world", textDirection:TextDirection.ltr)
+
+));
+
+}
